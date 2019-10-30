874b966fb146ddd1d6aba6cd0794a886f2a7c313
Ian Mathews <ian@redivis.com>
2019-10-30 14:10:17 -0700

    Update setup.py

--- pandas_gbq/setup.py
+++ pandas_gbq/setup.py
@@ -13,8 +13,7 @@ version = "0.0.1"
 release_status = "Development Status :: 3 - Alpha"
 dependencies = [
     "pandas-gbq == 0.11.0",
-    # "redivis-bigquery == 0.0.1"
-    "redivis-bigquery @ https://github.com/redivis/redipy.git#subdirectory=bigquery&egg=redivis-bigquery-0.0.1"
+    "redivis-bigquery @ git+https://github.com/redivis/redipy.git#subdirectory=bigquery"
  ]
 
 setup(
