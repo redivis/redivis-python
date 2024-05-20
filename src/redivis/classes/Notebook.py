e742cd7e8e893ee5670df548c102dbe5be814168
Ian Mathews <ian@redivis.com>
2024-05-20 13:49:43 -0700

    refactor: remove ability to pass string to notebook.create_output_Table

--- src/redivis/classes/Notebook.py
+++ src/redivis/classes/Notebook.py
@@ -19,38 +19,35 @@ class Notebook(Base):
         self.current_notebook_job_id = current_notebook_job_id
 
     def create_output_table(self, data=None, *, name=None, append=False, geography_variables=None):
-        if type(data) == str:
-            temp_file_path = str
+        temp_file_path = f"/tmp/redivis/out/{uuid.uuid4()}"
+        pathlib.Path(temp_file_path).parent.mkdir(exist_ok=True, parents=True)
+
+        import geopandas
+        import pandas as pd
+
+        if isinstance(data, geopandas.GeoDataFrame):
+            if geography_variables is None:
+                geography_variables = list(data.select_dtypes('geometry'))
+            data.to_wkt().to_parquet(path=temp_file_path, index=False)
+        elif isinstance(data, pd.DataFrame):
+            data.to_parquet(path=temp_file_path, index=False)
+        elif isinstance(data, pa_dataset.Dataset):
+            pa_dataset.write_dataset(data, temp_file_path, format='parquet', basename_template='part-{i}.parquet', max_partitions=1)
+            temp_file_path = f'{temp_file_path}/part-0.parquet'
+        elif isinstance(data, pa.Table):
+            pa_parquet.write_table(data, temp_file_path)
         else:
-            temp_file_path = f"/tmp/redivis/out/{uuid.uuid4()}"
-            pathlib.Path(temp_file_path).parent.mkdir(exist_ok=True, parents=True)
-            
-            import geopandas
-            import pandas as pd
-
-            if isinstance(data, geopandas.GeoDataFrame):
-                if geography_variables is None:
-                    geography_variables = list(data.select_dtypes('geometry'))
-                data.to_wkt().to_parquet(path=temp_file_path, index=False)
-            elif isinstance(data, pd.DataFrame):
-                data.to_parquet(path=temp_file_path, index=False)
-            elif isinstance(data, pa_dataset.Dataset):
-                pa_dataset.write_dataset(data, temp_file_path, format='parquet', basename_template='part-{i}.parquet', max_partitions=1)
-                temp_file_path = f'{temp_file_path}/part-0.parquet'
-            elif isinstance(data, pa.Table):
-                pa_parquet.write_table(data, temp_file_path)
+            # importing polars is causing an IllegalInstruction error on ARM + Docker. Import inline to avoid crashes elsewhwere
+            # TODO: revert once fixed upstream
+            import polars
+            if isinstance(data, polars.LazyFrame):
+                data.sink_parquet(temp_file_path)
+            elif isinstance(data, polars.DataFrame):
+                data.write_parquet(temp_file_path)
             else:
-                # importing polars is causing an IllegalInstruction error on ARM + Docker. Import inline to avoid crashes elsewhwere
-                # TODO: revert once fixed upstream
-                import polars
-                if isinstance(data, polars.LazyFrame):
-                    data.sink_parquet(temp_file_path)
-                elif isinstance(data, polars.DataFrame):
-                    data.write_parquet(temp_file_path)
-                else:
-                    raise Exception('Unknown datatype provided to notebook.create_output_table. Must either by a file path, or an instance of pandas.DataFrame, pyarrow.Dataset, pyarrow.Table, dask.DataFrame, polars.LazyFrame, or polars.DataFrame')
+                raise Exception('Unknown datatype provided to notebook.create_output_table. Must either by a file path, or an instance of pandas.DataFrame, pyarrow.Dataset, pyarrow.Table, dask.DataFrame, polars.LazyFrame, or polars.DataFrame')
 
-        size = os.stat(data.name).st_size if hasattr(data, "read") else len(data)
+        size = os.stat(temp_file_path).st_size
 
         pbar_bytes = tqdm(total=size, unit='B', leave=False, unit_scale=True)
 
@@ -62,11 +59,13 @@ class Notebook(Base):
         temp_upload = res["results"][0]
         print(temp_upload)
         if temp_upload["resumable"]:
-            perform_resumable_upload(data=data, progressbar=pbar_bytes,
+            perform_resumable_upload(data=temp_file_path, progressbar=pbar_bytes,
                                      temp_upload_url=temp_upload["url"])
         else:
-            perform_standard_upload(data=data, temp_upload_url=temp_upload["url"],
+            perform_standard_upload(data=temp_file_path, temp_upload_url=temp_upload["url"],
                                     progressbar=pbar_bytes)
+
+        os.remove(temp_file_path)
 
         res = make_request(
             method="PUT",
@@ -75,8 +74,5 @@ class Notebook(Base):
         )
 
         pbar_bytes.close()
-
-        if type(data) != str:
-            os.remove(temp_file_path)
 
         return Table(name=res["name"], properties=res)
